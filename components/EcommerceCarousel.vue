3436ea915270e8134c91441c82d44af3f2b14f65
Alexander Gusev <san4es021@gmail.com>
2019-08-05 15:38:43 +0300

    Home

--- components/EcommerceCarousel.vue
+++ components/EcommerceCarousel.vue
@@ -1,9 +1,13 @@
 <template lang="pug">
-  v-carousel(continuous cycle hide-delimiters width='100%' height='100%').ecommerce
-    v-carousel-item
-      v-img(src='/img/home/ecommerce.png' alt='ecommerce')
-    v-carousel-item
-      v-img(src='/img/home/ecommerce.png' alt='ecommerce')
+  div.ecommerce-carousel
+    v-carousel(continuous ref='carousel' cycle hide-delimiters width='100%' height='100%').ecommerce
+      v-carousel-item
+        v-img(src='/img/home/ecommerce.png' alt='ecommerce')
+      v-carousel-item
+        v-img(src='/img/home/ecommerce.png' alt='ecommerce')
+    .navigation
+      v-btn(@click='prev()' icon elevation='24').prev: v-icon mdi-chevron-left 
+      v-btn(@click='next()' icon elevation='24').next: v-icon mdi-chevron-right 
 
 </template>
 <script>
@@ -11,7 +15,18 @@ export default {
   props: {
     images: Array
   },
-  computed: {}
+  methods: {
+    next() {
+      const carousel = this.$refs.carousel.$el
+      const nextButton = carousel.children[0].children[3].children[0]
+      nextButton.click()
+    },
+    prev() {
+      const carousel = this.$refs.carousel.$el
+      const prevButton = carousel.children[0].children[2].children[0]
+      prevButton.click()
+    }
+  }
 }
 </script>
 <style lang="sass">
--- pages/_lang/index.vue
+++ pages/_lang/index.vue
@@ -23,9 +23,16 @@
                   input(placeholder='To').input
                   v-icon mdi-chevron-right
               .search-group
-                .search-item.button
-                  span.label Dates
-                  v-icon mdi-calendar-multiple
+                v-menu(ref="menu" v-model="menu" transition="scale-transition"  :close-on-content-click="false")
+                    template(v-slot:activator="{ on }")
+                      .search-item.button(v-on='on')
+                        span.label Dates
+                        v-icon mdi-calendar-multiple
+                    v-date-picker(v-model="date" no-title width='266')
+                      v-spacer
+                      v-btn(text color="primary" @click="menu = false") Cancel
+                      v-btn(text color="primary" @click="$refs.menu.save(date)") OK
+                
                 .search-item.button
                   span.label Event size
                   v-icon mdi-human-male
@@ -118,6 +125,12 @@ import FooterComponent from "@/components/Footer"
 import EcommerceCarousel from "@/components/EcommerceCarousel"
 
 export default {
+  data() {
+    return {
+      menu: null,
+      date: null
+    }
+  },
   components: {
     HeaderComponent,
     FooterComponent,
